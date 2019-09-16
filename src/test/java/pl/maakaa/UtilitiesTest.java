257341d51b1e6be8e4601d389f93f981276185f2
Marzena <kacprowicz.marzena@gmail.com>
2019-09-16 20:58:51 +0200

    create UtilitiesTest class with fail() methods.

---
+++ src/test/java/pl/maakaa/UtilitiesTest.java
@@ -0,0 +1,28 @@
+package pl.maakaa;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UtilitiesTest {
+
+    @Test
+    public void everyNthChar() {
+        fail("Test failed");
+    }
+
+    @Test
+    public void removePairs() {
+        fail("Test failed");
+    }
+
+    @Test
+    public void converter() {
+        fail("Test failed");
+    }
+
+    @Test
+    public void nullIfOddLength() {
+        fail("Test failed");
+    }
+}
