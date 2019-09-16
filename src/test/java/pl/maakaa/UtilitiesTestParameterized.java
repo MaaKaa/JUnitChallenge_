cdbd8d27f029e328041b9330b35f27df60c3e08e
Marzena <kacprowicz.marzena@gmail.com>
2019-09-16 21:50:14 +0200

    create UtilitiesTestParameterized class.

---
+++ src/test/java/pl/maakaa/UtilitiesTestParameterized.java
@@ -0,0 +1,45 @@
+package pl.maakaa;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class UtilitiesTestParameterized {
+
+    private Utilities utilities;
+
+    private String input;
+    private String output;
+
+    public UtilitiesTestParameterized(String input, String output) {
+        this.input = input;
+        this.output = output;
+    }
+
+    @Before
+    public void setup(){
+        utilities = new Utilities();
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> testConditions(){
+        return Arrays.asList(new Object[][] {
+                {"ABCDEFF", "ABCDEF"},
+                {"AB88EFFG", "AB8EFG"},
+                {"112233445566", "123456"},
+                {"A", "A"}
+        });
+    }
+
+    @Test
+    public void removePairs() throws  Exception {
+        assertEquals(output, utilities.removePairs(input));
+    }
+}
